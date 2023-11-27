467008065a387ecf850ec754bf4f1528f3c59923
migueltomarnogueira <tomarnogueiracoding@gmail.com>
2023-11-27 15:24:16 +0000

    reference vs value

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-let c = 3
+let a = 10
+let b = 'Hi'
+let c = [1, 2] // memory adress 0x01
+let d = c // memory adress 0x02
+d.push(3)
 
-{
-  let a = 1
-  console.log(a)
-  console.log(c)
-  {
-    let b = 2
-    console.log(a)
-    console.log(b)
-    console.log(c)
+console.log('a = ' + a)
+console.log('b = ' + b)
+console.log('c = ' + c)
+console.log('d = ' + d)
 
-    {
-      let d = 4
-      console.log(a)
-      console.log(b)
-      console.log(c)
-      console.log(d)
-    }
-    
-  }
+let e = [1, 2] // memory adress 0x03
+let f = [1, 2] // memory adress 0x04
 
+console.log(e === f) // returns false cause the value refers to the adress in memory
+
+const g = [1, 2] // memory adress 0x05
+const elementToAdd = 3 // 3
+
+add(g, elementToAdd) // 0x05, 3
+
+console.log(g)
+console.log(elementToAdd)
+
+function add(array, element) {
+  element = element + 1 // 4
+  array.push(element) // memory adress 0x05
 }
-
-console.log(c)
-
 
