30fa29e30d6b6a0c038b4423991e7d75817fba99
migueltomarnogueira <tomarnogueiracoding@gmail.com>
2024-05-09 14:37:23 +0100

    form validation complete

--- form-validation/script.js
+++ form-validation/script.js
@@ -6,21 +6,41 @@ const usernameInput = document.querySelector('#username')
 const passwordInput = document.querySelector('#password')
 const passwordConfirmInput = document.querySelector('#password-confirmation')
 const termsInput = document.querySelector('#terms')
+const errorsContainer = document.querySelector('.errors')
+const errorsList = document.querySelector('.errors-list')
 const btn = document.querySelector('button')
 
 // TODO: Create an event listener for when the form is submitted and do the following inside of it.
 form.addEventListener('submit', (e) => {
-  e.preventDefault()
   //    TODO: Create an array to store all error messages and clear any old error messages
-  const errorMessages = []
-})
-//    TODO: Define the following validation checks with appropriate error messages
-//      1. Ensure the username is at least 6 characters long
-//      2. Ensure the password is at least 10 characters long
-//      3. Ensure the password and confirmation password match
-//      4. Ensure the terms checkbox is checked
-//    TODO: If there are any errors then prevent the form from submitting and show the error messages
 
+  const errorMessages = []
+  clearErrors()
+
+  //    TODO: Define the following validation checks with appropriate error messages
+  //      1. Ensure the username is at least 6 characters long
+  if (usernameInput.value.length < 6) {
+    errorMessages.push('Username must be at least six characters')
+  }
+  //      2. Ensure the password is at least 10 characters long
+  if (passwordInput.value.length < 10) {
+    errorMessages.push('Password must be at least ten characters')
+  }
+  //      3. Ensure the password and confirmation password match
+  if (passwordInput.value !== passwordConfirmInput.value) {
+    errorMessages.push('Password must match')
+  }
+  //      4. Ensure the terms checkbox is checked
+  if (!termsInput.checked) {
+    errorMessages.push('You must agree to the terms')
+  }
+  //    TODO: If there are any errors then prevent the form from submitting and show the error messages
+
+  if (errorMessages.length > 0) {
+    showErrors(errorMessages)
+    e.preventDefault()
+  }
+})
 // TODO: Define this function
 function clearErrors() {
   // Loop through all the children of the error-list element and remove them
@@ -28,6 +48,10 @@ function clearErrors() {
   // I recommend using a while loop to accomplish this task
   // This is the trickiest part of this exercise so if you get stuck and are unable to progress you can also set the innerHTML property of the error-list to an empty string and that will also clear the children. I recommend trying to accomplish this with a while loop, though, for practice.
   // Also, make sure you remove the show class to the errors container
+  while (errorsList.children[0] != null) {
+    errorsList.removeChild(errorsList.children[0])
+  }
+  errorsContainer.classList.remove('show')
 }
 
 // TODO: Define this function
@@ -35,4 +59,10 @@ function showErrors(errorMessages) {
   // Add each error to the error-list element
   // Make sure to use an li as the element for each error
   // Also, make sure you add the show class to the errors container
+  errorMessages.forEach((errorMessage) => {
+    const li = document.createElement('li')
+    li.innerText = errorMessage
+    errorsList.appendChild(li)
+  })
+  errorsContainer.classList.add('show')
 }
