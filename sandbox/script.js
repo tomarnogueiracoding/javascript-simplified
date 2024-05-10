2c7c77b02321151d7fce33ca260130044d66d4af
migueltomarnogueira <tomarnogueiracoding@gmail.com>
2024-05-10 20:08:57 +0100

    sandbox - promises training

--- sandbox/script.js
+++ sandbox/script.js
@@ -1,9 +1,53 @@
-const person = {
-  name: 'John',
-  age: 30,
-  city: 'New York',
+// setTimeout(() => {
+//   console.log('1')
+//   setTimeout(() => {
+//     console.log('2')
+//     setTimeout(() => {
+//       console.log('3')
+//     }, 250)
+//   }, 250)
+// }, 250)
+
+setTimeOutPromise(250)
+  .then(() => {
+    console.log('1')
+    return setTimeOutPromise(250)
+  })
+  .then(() => {
+    console.log('2')
+    return setTimeOutPromise(250)
+  })
+  .then(() => {
+    console.log('3')
+  })
+
+function setTimeOutPromise(duration) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, duration)
+  })
 }
 
-console.log(name)
-console.log(age)
-console.log(city)
+const button = document.querySelector('button')
+
+function addEventListener(element, method) {
+  return new Promise((resolve, reject) => {
+    element.addEventListener(method, resolve)
+  })
+}
+
+addEventListener(button, 'click').then((e) => {
+  console.log('clicked')
+  console.log(e)
+})
+
+Promise.all([
+  Promise.resolve('1'),
+  Promise.reject('Error on 2'),
+  Promise.resolve('3'),
+])
+  .then((message) => {
+    console.log(message)
+  })
+  .catch((error) => {
+    console.error(error)
+  })
